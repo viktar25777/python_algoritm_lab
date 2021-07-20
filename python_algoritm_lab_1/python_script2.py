8397f5b2bf4d2d58c94d3b7c8ffabea7af8414d2
viktar25777 <viktar99999@gmail.com>
2021-07-20 14:59:54 +0300

    one homework

---
+++ python_algoritm_lab_1/python_script2.py
@@ -0,0 +1,14 @@
+a = 7
+y = 7
+def coordinats(a, y):
+    try:
+        a = int(input("Введите целое число: "))
+        k = 1
+        b = 0
+    except zerodivisionerror:
+        return
+    y = k * a + b
+    return y
+
+print(coordinats(a,y))
+
