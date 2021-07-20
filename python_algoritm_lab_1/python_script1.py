d64a7640e2ef07119ba9b2568789cfdd57f181c2
viktar25777 <viktar99999@gmail.com>
2021-07-20 14:34:38 +0300

    one homework

---
+++ python_algoritm_lab_1/python_script1.py
@@ -0,0 +1,14 @@
+a = int(input("Введите целое число: "))
+b = int(input("Введите целое число: "))
+c = a & b
+print(c)
+d = int(input("Введите целое число: "))
+e = int(input("Введите целое число: "))
+f = d | e
+print(f)
+g = int(input("Введите целое число: "))
+k = g<<2
+print(k)
+h = int(input("Введите целое число: "))
+l = h>>2
+print(l)
