93139ea4d98b7495ef089d5e911ffb4f7bad2eeb
viktar25777 <viktar99999@gmail.com>
2021-07-28 22:25:15 +0300

    third homework

---
+++ python_algoritm_lab_3/python_script8.py
@@ -0,0 +1,17 @@
+import random
+from random import randint
+m, n, y, z= [int(input(i ))for i in ("n = ","m = ","y = ","z = ")]
+matrix = [[randint(y, z) for _ in range (n)]for k in range (m)]
+print(matrix)
+for i in matrix:
+    print(*i)
+
+for x in range(m):
+    print(min(matrix[x]))
+
+a = min(matrix[x])
+for x in range(m):
+    list_col = [a for i in range(m) if a > min(matrix[x])]
+    print(list_col)
+
+
